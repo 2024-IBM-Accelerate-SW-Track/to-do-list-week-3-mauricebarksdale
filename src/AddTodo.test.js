71260e2c4798f276e2d570e09169252c3c21d845
mauricebarksdale <momobarksdale@gmail.com>
2024-06-24 22:26:58 -0400

    Implement first draft of tests

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -21,24 +21,81 @@ afterEach(() => {
 
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
-  
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "07/30/2024";
+  let taskCount = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (!(screen.getByText("History Test"))) {
+      taskCount++;
+    }
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.change(inputDate, { target: { value: dueDate}});
+    fireEvent.click(element);
+  }
+
+  expect(taskCount).toBe(1);
  });
 
- test('test that App component doesn\'t add a task without task name', () => {
+test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "07/30/2024";
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.queryByText(/History Test/i);
+  const checkDate = screen.queryByText(new RegExp(dueDate, "i"));
+  expect(check).not.toBeInTheDocument();
+  expect(checkDate).not.toBeInTheDocument();
+});
 
- test('test that App component doesn\'t add a task without due date', () => {
-  render(<App />);
- });
+  test('test that App component doesn\'t add a task without due date', () => {
+    render(<App />);
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+    const addButton = screen.getByRole('button', {name: /Add/i});
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.click(addButton);
+    const task = screen.queryByText(/History Test/i);
+    expect(task).not.toBeInTheDocument();
+  });
 
 
 
- test('test that App component can be deleted thru checkbox', () => {
-  render(<App />);
- });
+  test('test that App component can be deleted thru checkbox', () => {
+    render(<App />);
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+    const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+    const element = screen.getByRole('button', {name: /Add/i});
+    const dueDate = "07/30/2024";
+    const checkbox = screen.getByRole('checkbox');
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.change(inputDate, { target: { value: dueDate}});
+    fireEvent.click(element);
+    fireEvent.click(checkbox);
+    const check = screen.getByText(/History Test/i);
+    const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+    expect(check).not.toBeInTheDocument();
+    expect(checkDate).not.toBeInTheDocument();
+  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/History Test/i);
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  const taskElement = screen.getByText(/History Test/i).closest('.task');
+  const backgroundColor = window.getComputedStyle(taskElement).backgroundColor;
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
+  expect(backgroundColor).toBe('red');
  });
--- src/component/todos.js
+++ src/component/todos.js
@@ -21,7 +21,7 @@ const Todos = ({ todos, deleteTodo }) => {
 
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10, background:color}} data-testid={todo.content}>
+          <Card style={{marginTop:10, background:color}} data-testid="new-item-input">
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
