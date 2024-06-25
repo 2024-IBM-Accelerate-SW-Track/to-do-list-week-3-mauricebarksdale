78f7eb5c2d190db93b55b1bdc50860185564fec7
mauricebarksdale <momobarksdale@gmail.com>
2024-06-24 23:27:20 -0400

    Change test-id from textfield to input

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -59,7 +59,7 @@ class AddTodo extends Component {
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
-          data-testid="new-item-textfield"
+          data-testid="new-item-input"
         />
         <LocalizationProvider dateAdapter={AdapterDateFns}>         
           <DesktopDatePicker
