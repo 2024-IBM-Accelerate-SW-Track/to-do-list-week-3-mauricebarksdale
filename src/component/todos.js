95e15bdfd63d4e15e2fbf2c40c809e751a395feb
mauricebarksdale <momobarksdale@gmail.com>
2024-06-24 22:55:52 -0400

    Revised test for determining if task was overdue

--- src/component/todos.js
+++ src/component/todos.js
@@ -21,13 +21,13 @@ const Todos = ({ todos, deleteTodo }) => {
 
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10, background:color}} data-testid="new-item-input">
+          <Card style={{marginTop:10, background:color}} data-testid={todo.content}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
             <ListItemButton component="a" href="#simple-list">
               <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
-              <ListItemText primary={todo.content} secondary={todo.due}/>
+              <ListItemText primary={todo.content} secondary={todo.due} />
             </ListItemButton>
           </Card>
         </Grid>
